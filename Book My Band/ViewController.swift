8c592c8a425c7d61fe9ddf88facd5e00ef6c571a
Matt Huston <matthuston1@aol.com>
2018-09-13 09:55:17 -0600

    Initial Commit

---
+++ Book My Band/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Book My Band
+//
+//  Created by Matt Huston on 9/13/18.
+//  Copyright © 2018 Matt Huston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
